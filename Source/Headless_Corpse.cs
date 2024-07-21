7eec7847f626bcfd133fc368323014c24ac84708
Braeunig <160392333+Braeunig@users.noreply.github.com>
2024-07-21 19:15:13 +0200

    added .dll file, general cleanup

--- Source/Headless_Corpse.cs
+++ Source/Headless_Corpse.cs
@@ -13,9 +13,8 @@ namespace HeadlessCorpseFilter {
     public class SpecialThingFilterWorker_HeadlessCorpse : SpecialThingFilterWorker {
         public override bool Matches(Thing t) {
             Corpse corpse = t as Corpse;
-            return corpse.InnerPawn.health.hediffSet.PartIsMissing((BodyPartRecord p) => p.def == BodyPartDefOf.Head)
+            return corpse.InnerPawn.health.hediffSet.GetNotMissingParts().Any((BodyPartRecord p) => p.def == BodyPartDefOf.Head);
+          
         }
     }
 }
-
-
